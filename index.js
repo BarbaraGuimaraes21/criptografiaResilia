b3dda3744e2ffee3dfdbea50dac5d98af591b69f
BarbaraGuimaraes21 <bahcamargo21@gmail.com>
2022-05-04 19:16:52 -0300

    Alterações no JavaScript

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var select = document.querySelector(".selecao");
 var passo = document.querySelector("#passo");
 
-select.addEventListener('click', function(){
+select.addEventListener('click', function () {
     if (select.value == "cesar") {
         passo.style.display = "flex";
     } else {
@@ -13,48 +13,48 @@ var btn = document.querySelector('.codifica')
 btn.addEventListener('click', function (e) {
     e.preventDefault()
 
-    if(select.value =="cesar"){
-        
-            var incremento = document.getElementById("chave").value;
-            incremento = incremento % 26; 
-        
-            if (document.querySelector("#decodificar").checked) {
+    if (select.value == "cesar") {
+
+        var incremento = document.getElementById("chave").value;
+        incremento = incremento % 26;
+
+        if (document.querySelector("#decodificar").checked) {
             incremento = incremento * -1;
-            }
-            cesar(incremento); 
-        
+        }
+        cesar(incremento);
+
         function cesar(incremento) {
-            var textoDeEntrada = document.querySelector(".entrada").value;
-            var textoDeSaida = document.querySelector(".saida");
-            var transiçao = ""; 
-            textoDeSaida.innerHTML = ""; 
+            var textoDeEntrada = document.querySelector(".mensagem").value;
+            var textoDeSaida = document.querySelector(".resultado");
+            var transicao = "";
+            textoDeSaida.innerHTML = "";
             for (var i = 0; i < textoDeEntrada.length; i++) {
-            transiçao = textoDeEntrada.charCodeAt(i); 
-            if (transiçao >= 65 && transiçao <= 90) {
-                transiçao = transiçao + incremento; 
-                if (transiçao > 90) {
-                transiçao = transiçao - 26; 
-                } else if (transiçao < 65) {
-                transiçao = transiçao + 26; 
+                transicao = textoDeEntrada.charCodeAt(i);
+                if (transicao >= 65 && transicao <= 90) {
+                    transicao = transicao + incremento;
+                    if (transicao > 90) {
+                        transicao = transicao - 26;
+                    } else if (transicao < 65) {
+                        transicao = transicao + 26;
+                    }
                 }
-            }
-            if (transiçao >= 97 && transiçao <= 122) {
-                transiçao = transiçao + incremento; 
-                if (transiçao > 122) {
-                transiçao = transiçao - 26; 
-                } else if (transiçao < 97) {
-                transiçao = transiçao + 26; 
+                if (transicao >= 97 && transicao <= 122) {
+                    transicao = transicao + incremento;
+                    if (transicao > 122) {
+                        transicao = transicao - 26;
+                    } else if (transicao < 97) {
+                        transicao = transicao + 26;
+                    }
                 }
-            }
-            textoDeSaida.innerHTML += String.fromCharCode(transiçao); 
+                textoDeSaida.innerHTML += String.fromCharCode(transicao);
             }
         }
-    }else if(select.value == 'b64'){
+    } else if (select.value == 'base64') {
 
-    var textoDeEntrada = document.querySelector(".entrada").value;
-    var textoDeSaida = document.querySelector(".saida");
-    if (document.querySelector("#codificar").checked)
-    textoDeSaida.innerHTML = btoa(textoDeEntrada);
-    else textoDeSaida.innerHTML = atob(textoDeEntrada);
-}
+        var textoDeEntrada = document.querySelector(".mensagem").value;
+        var textoDeSaida = document.querySelector(".resultado");
+        if (document.querySelector("#codificar").checked)
+            textoDeSaida.innerHTML = btoa(textoDeEntrada);
+        else textoDeSaida.innerHTML = atob(textoDeEntrada);
+    }
 })
